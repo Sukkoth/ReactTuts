42ec48d9f9a92930776faabcc8c4ee7bdb1e785e
Sukkoth <suukootj@gmail.com>
2023-07-06 15:53:15 +0300

    delete unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
 import NoteApp from './NoteApp';
-import { useDispatch } from 'react-redux';
-import { fetchNotesAction } from './redux/actions/notesActions';
 
 function App() {
     return <NoteApp />;
