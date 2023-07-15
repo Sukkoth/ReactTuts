1321eb5fbc9483e3357e0b60c75c40b7fa3aaca3
Sukkoth <suukootj@gmail.com>
2023-07-15 10:28:44 +0300

    on Ep-37 2:30 added ccount id

--- src/redux/slice/users/usersSlice.js
+++ src/redux/slice/users/usersSlice.js
@@ -2,6 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import baseURL from '../../../utils/baseURL';
 
+//initial state
 const initialState = {
     loading: false,
     error: null,
